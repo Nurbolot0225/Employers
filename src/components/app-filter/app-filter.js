a8084409ee3d727b60c8961f564666d99dcb6887
Nurbolot Berdibekov <nurchik0225@gmail.com>
2022-11-30 17:12:08 +0600

    Fixed bug rise

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,14 +2,15 @@ import "./app-filter.scss";
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: 'all', label: 'Все сотрудники'},
-        {name: 'rise', label: 'На повышение'},
-        {name: 'moreThen1000', label: 'З/П больше 1000$'}
+        {name: 'all', label: 'Все сотрудники', colored: false},
+        {name: 'rise', label: 'На повышение', colored: false},
+        {name: 'moreThen1000', label: 'З/П больше 1000$', colored: true}
     ];
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light';
+        // const style = colored ? {color: 'red'} : null    
         return (
             <button type="button"
                     className={`btn ${clazz}`}
